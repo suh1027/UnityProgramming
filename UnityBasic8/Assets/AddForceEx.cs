910f843295a98b79583b9b677be843308fca07ab
서지훈 <suh1027@naver.com>
2020-04-03 13:10:23 +0900

    UnityBasic8
        1. 실제 물리적인 충돌로 발생하는 이벤트
    
        void OnCollisionEnter(Collision collision)
        void OnCollisionStay(Collision collision)
        void OnCollisionExit(Collision collision)
    
    
        2. Collider 충돌로 발생하는 이벤트 
        (충돌대상이 되는 Material에 BoxCollider에 Is Trigger 체크 ex> Cube)
    
        void OnTriggerEnter(Collider other)
        void OnTriggerStay(Collider other)
        void OnTriggerExit(Collider other)

---
+++ UnityBasic8/Assets/AddForceEx.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceEx : MonoBehaviour
+{
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //rigidbody 초기화
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 vt3 = new Vector3(
+            Input.GetAxis("Horizontal"),
+            0,
+            Input.GetAxis("Vertical"));
+        rb.AddForce(vt3, ForceMode.Impulse);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.name == "Cube")
+        {
+            rb.AddForce(Vector3.up * 2, ForceMode.Impulse);
+        }
+    }
+
+    /* 정리 
+    
+    1. 실제 물리적인 충돌로 발생하는 이벤트
+
+    void OnCollisionEnter(Collision collision)
+    void OnCollisionStay(Collision collision)
+    void OnCollisionExit(Collision collision)
+
+
+    2. Collider 충돌로 발생하는 이벤트 
+    (충돌대상이 되는 Material에 BoxCollider에 Is Trigger 체크 ex> Cube)
+
+    void OnTriggerEnter(Collider other)
+    void OnTriggerStay(Collider other)
+    void OnTriggerExit(Collider other)
+     
+    */
+
+}
---
+++ UnityBasic8/Assets/OtherBalls.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherBalls : MonoBehaviour
+{
+    MeshRenderer mesh;
+    Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        mat = mesh.material;
+    }
+
+    // # 물리충돌 이벤트 
+    
+    // OncollisionEnter -> 물리충돌이 시작될때 호출되는 함수.
+    private void OnCollisionEnter(Collision collision) 
+        //Collision 충돌정보 클래스
+    {
+        if(collision.gameObject.name == "CenterBall") { 
+            // color : 기본색상 클래스, color32 : 255 색상 클래스
+            mat.color = new Color(0, 0, 0); //(r,g,b)
+        }
+    }
+
+    
+    // OnCollisionStay -> 물리적충돌이 지속될떄 호출되는 함수
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+
+    // OnCollisionExit -> 물리적 충돌이 끝났을때 호출되는 함수.
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.name == "CenterBall")
+        {
+            mat.color = new Color(1, 1, 1);//흰색
+        }
+    }
+    
+
+}
