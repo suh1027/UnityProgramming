27f928bb19c1290e84d5f1c3a25d26d6eaf477ac
서지훈 <suh1027@naver.com>
2020-04-03 14:28:02 +0900

    UnityBasic9
    UGUI 기초 실습

---
+++ UnityBasic9/Assets/MyBall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBall : MonoBehaviour
+{
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector3 vt = new Vector3(h, 0, v);
+
+        rb.AddForce(vt, ForceMode.Impulse);
+    }
+
+    public void Jump()
+    {
+        rb.AddForce(Vector3.up * 20, ForceMode.Impulse);
+    }
+
+}
