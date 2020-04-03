4491d4f3dbed229a648483c6903a6869fec3176a
서지훈 <suh1027@naver.com>
2020-04-03 17:56:44 +0900

    Unity 2D Example 1-2
    2D 게임 프로젝트 공부

---
+++ Unity 2D Example/Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        //float v = Input.GetAxis("Vertical");
+        float j = Input.GetAxis("Jump");
+        
+        Vector2 vt = new Vector2(h,j);
+
+        rb.AddForce(vt, ForceMode2D.Impulse);
+    }
+}
