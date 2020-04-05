5a9b198e89b379afe01f9994f6e823ee5c62150f
서지훈 <suh1027@naver.com>
2020-04-06 00:00:58 +0900

    Unity 2D Rpg Project
    Collider 수정 필

---
+++ Unity 2D RPG Project/Assets/Script/PlayerAction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    public float speed;
+
+    float h;
+    float v;
+
+    Rigidbody2D r2d;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+    }
+    private void FixedUpdate()
+    {
+        r2d.velocity = new Vector2(h, v) * 2;
+    }
+}
