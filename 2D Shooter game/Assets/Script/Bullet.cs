af9832416d4dae6f19f04571c6e8b3b656fb6107
서지훈 <suh1027@naver.com>
2020-04-08 21:44:47 +0900

    Bullet Action 추가 Power1,2,3

---
+++ 2D Shooter game/Assets/Script/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "BorderBullet")
+        {
+            Destroy(gameObject); // Monobehaviour의 기본 메소드 : 오브젝트 삭제
+        }
+    }
+}
--- 2D Shooter game/Assets/Script/Player.cs
+++ 2D Shooter game/Assets/Script/Player.cs
@@ -6,10 +6,18 @@ public class Player : MonoBehaviour
 {
     //public 변수
     public float speed;
+    //bullet 속도제한 변수
+    public float maxShotDelay; // 최대
+    public float curShotDelay; // 현재
+    public float power;
+
     public bool isTouchTop;
     public bool isTouchBot;
     public bool isTouchLeft;
     public bool isTouchRight;
+
+    public GameObject bulletA;
+    public GameObject bulletB;
     
     Animator animator;
 
@@ -20,8 +28,15 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        Move();
+        Fire();
+        Reload();
+    }
+
+    void Move()
+    {
         float h = Input.GetAxisRaw("Horizontal");
-        if ((h == 1 && isTouchRight)||(h == -1 && isTouchLeft))
+        if ((h == 1 && isTouchRight) || (h == -1 && isTouchLeft))
             h = 0;
 
         float v = Input.GetAxisRaw("Vertical");
@@ -38,6 +53,59 @@ public class Player : MonoBehaviour
             animator.SetInteger("Input", (int)h);
 
     }
+
+    void Fire()
+    {
+        //총알 충돌하지 않도록 isTrigger 키기
+        //Instantiate 매개변수 오브젝트를 생성하는 함수
+        if (!Input.GetButton("Fire1"))
+            return;
+
+        if (curShotDelay < maxShotDelay)
+            return;
+
+        switch (power)
+        {
+            case 1:
+                //power = 1
+                GameObject bullet = Instantiate(bulletA, transform.position, transform.rotation);
+                Rigidbody2D r2d = bullet.GetComponent<Rigidbody2D>();
+                r2d.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 2:
+                //power = 2
+                //power = 1일때
+                GameObject bulletR = Instantiate(bulletA, transform.position + Vector3.right * 0.1f, transform.rotation);
+                GameObject bulletL = Instantiate(bulletA, transform.position + Vector3.left * 0.1f, transform.rotation);
+                Rigidbody2D r2dR = bulletR.GetComponent<Rigidbody2D>();
+                Rigidbody2D r2dL = bulletL.GetComponent<Rigidbody2D>();
+                r2dR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                r2dL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+            case 3:
+                GameObject bulletRR = Instantiate(bulletA, transform.position + Vector3.right * 0.35f, transform.rotation);
+                GameObject bulletCC = Instantiate(bulletB, transform.position, transform.rotation); //가운데를 크게! BulletBtype 사용
+                GameObject bulletLL = Instantiate(bulletA, transform.position + Vector3.left * 0.35f, transform.rotation);
+                Rigidbody2D r2dRR = bulletRR.GetComponent<Rigidbody2D>();
+                Rigidbody2D r2dCC = bulletCC.GetComponent<Rigidbody2D>();
+                Rigidbody2D r2dLL = bulletLL.GetComponent<Rigidbody2D>();
+                r2dRR.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                r2dCC.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                r2dLL.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                break;
+
+
+        }
+
+
+        curShotDelay = 0;
+    }
+
+    void Reload()
+    {
+        curShotDelay += Time.deltaTime; //현재시간
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Border")
