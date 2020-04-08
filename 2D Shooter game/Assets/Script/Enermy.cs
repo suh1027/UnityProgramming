9b154f36c5971999bb6499ea90a461e21e2bd0b9
서지훈 <suh1027@naver.com>
2020-04-08 22:55:28 +0900

    3Type Enermy, Damage Action, Effect 추가

---
+++ 2D Shooter game/Assets/Script/Enermy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enermy : MonoBehaviour
+{
+    public float speed;
+    public int health;
+    public Sprite[] sprites;
+    // 피격시 색이 살짝 변하는 sprites 구현하기위해
+
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D r2d;
+
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        r2d = GetComponent<Rigidbody2D>();
+        r2d.velocity = Vector2.down * speed; //아래로 내려오도록 설정
+    }
+
+    void OnHit(int dmg)
+    {
+        health -= dmg;
+        spriteRenderer.sprite = sprites[1];
+        
+        Invoke("ReturnSprite", 0.1f); // 시간차(0.1초)를 두어 ReturnSprite()
+
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void ReturnSprite()
+    {
+        spriteRenderer.sprite = sprites[0];
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "BorderBullet")
+        {
+            Destroy(gameObject);
+        }
+        else if(collision.gameObject.tag == "PlayerBullet")
+        {
+            Bullet bullet = collision.gameObject.GetComponent<Bullet>();
+            OnHit(bullet.dmg);
+
+            //맞았을때 총알 제거
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ 2D Shooter game/Assets/Script/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Prefab 변수화
+    public GameObject[] enermyObjs;
+    public Transform[] spawnPoints; // 스폰될 위치를 transform 배열에 저장
+
+    public float maxSpawnDelay;
+    public float curSpawnDelay;
+
+    void Update()
+    {
+        curSpawnDelay += Time.deltaTime;
+
+        if(curSpawnDelay > maxSpawnDelay)
+        {
+            SpawnEnermy();
+            maxSpawnDelay = Random.Range(0.5f, 3f);
+            curSpawnDelay = 0;
+        }
+    }
+
+    void SpawnEnermy()
+    {
+        int ranEnermy = Random.Range(0, 3); // L M S 비행기 세가지 종류
+        int ranPoint = Random.Range(0, 5); //다섯가지 포인트
+        
+        //프리팹 생성
+        Instantiate(enermyObjs[ranEnermy],
+            spawnPoints[ranPoint].position,
+            spawnPoints[ranPoint].rotation);
+    }
+}
